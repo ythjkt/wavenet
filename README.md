4517eaa7739812b7f368252505fe065067be25be
ythjkt <ythjkt@gmail.com>
2022-07-05 22:01:47 +0900

    Make generate.py take arguments.

--- README.md
+++ README.md
@@ -21,3 +21,23 @@ the trained weights to disk.
 # Run python3 train.py --help to see all the options.
 python3 train.py <data dir> <result dir>
 ```
+
+## Generating audio.
+generate.py generates audio iteratively by making the model predict the value of
+the next timestamp. It starts with an arbitrary timestamp 1 and make the model
+predict the next timestamp. Then it appends the output of the model to the input
+makind the model generate one timestamp at a time.
+
+By default the script generates an audio clip of 5 seconds.
+
+```
+# Run python3 train.py --help to see all the options.
+python3 generate.py <weights_path> <output_dir>
+```
+
+## Running unit tests.
+Make sure these tests pass after modifying model.py or util.py.
+```
+pytest test_util.py
+python3 wavenet/model_test.py
+```
--- generate.py
+++ generate.py
@@ -1,18 +1,40 @@
-from wavenet.model import WaveNet
-import time
-import numpy as np
-import tensorflow as tf
-from preprocess import mulaw_quantize
-import time
-import json
 import os
-from scipy.io import wavfile
+import time
 import params
+import argparse
 from util import *
+from wavenet.model import WaveNet
+import tensorflow as tf
 
-SAMPLING_RATE = params.sampling_rate
-GENERATE_LEN = SAMPLING_RATE * 5
-CHECKPOINTS_DIR = './results/ckpts'  # TODO: Put this to params.py
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='WaveNet train script')
+    parser.add_argument('weights_path',
+                        type=str,
+                        help="path to weights of the model")
+    parser.add_argument('output_dir',
+                        type=str,
+                        help="path to directory for generated audio file")
+    parser.add_argument('-l',
+                        '--length',
+                        type=int,
+                        default=5,
+                        help='length of the generated audio in seconds')
+    parser.add_argument(
+        '-r',
+        '--sampling_rate',
+        type=int,
+        default=params.sampling_rate,
+        help="should match the sampling rate used with preproces.py")
+    args = parser.parse_args()
+
+    if args.length < 0:
+        raise ValueError('length needs to be positive')
+
+    if args.sampling_rate < 0:
+        raise ValueError('sampling_rate needs to be positive')
+
+    return args
 
 
 def print_progress_bar(iteration,
@@ -22,7 +44,7 @@ def print_progress_bar(iteration,
                        fill='#',
                        length=50,
                        print_end='\r'):
-    if iteration % frequency != 0:
+    if iteration % frequency != 0 and iteration != total:
         return
     percent = 100 * iteration / float(total)
     fill_len = int(length * iteration // total)
@@ -33,40 +55,44 @@ def print_progress_bar(iteration,
 
 
 def main():
-    os.makedirs('./generated/', exist_ok=True)
+    args = get_arguments()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    print(args.output_dir)
 
     # Initialize WaveNet.
     wavenet = WaveNet(params.dilations, params.filter_width,
                       params.residual_channels, params.dilation_channels,
                       params.skip_channels)
-    # latest = tf.train.latest_checkpoint(CHECKPOINTS_DIR)
+
     start_time = time.time()
-    wavenet.load_weights('results/weights/wavenet_10000')
+    wavenet.load_weights(args.weights_path)
 
     initial_value = mulaw_quantize(0)
     input = tf.one_hot(indices=initial_value, depth=256, dtype=tf.float32)
     input = tf.reshape(input, [1, 1, 256])
     outputs = []
+    generate_len = args.length * args.sampling_rate
     with tf.device('/cpu:0'):
-        for i in range(GENERATE_LEN):
+        for i in range(generate_len):
             input = wavenet.call(input, is_generate=True)
             index = tf.argmax(input, axis=-1)
             input = tf.reshape(tf.one_hot(indices=index, depth=256),
                                [1, 1, 256])
             outputs.append(tf.squeeze(tf.argmax(input, axis=-1)))
-            print_progress_bar(i, GENERATE_LEN)
+            print_progress_bar(i + 1, generate_len)
 
     outputs = tf.concat(outputs, axis=0)
     end_time = time.time()
     print(
-        f"Generating {GENERATE_LEN} took {end_time - start_time:.0f} seconds.")
+        f"Generating {generate_len} took {end_time - start_time:.0f} seconds.")
 
     outputs = outputs.numpy()
     outputs = inv_mulaw_quantize(outputs)
-    save_wav(outputs, f'./generated/audio-{time.strftime("%Y%m%d-%H%M%S")}.wav',
-             SAMPLING_RATE)
+    output_path = os.path.join(args.output_dir,
+                               f'audio-{time.strftime("%Y%m%d-%H%M%S")}.wav')
+    save_wav(outputs, output_path, args.sampling_rate)
 
 
 if __name__ == '__main__':
     main()
-    print('genedated')
